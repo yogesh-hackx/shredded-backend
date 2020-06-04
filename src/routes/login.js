3a5cbd6c3ed41e5b43196eee86cf6ffa1e8a07c6
Ashutosh619-sudo <Ashutoshsinghrkt@gmail.com>
2020-06-05 03:32:32 +0530

    add login endpoint

--- src/routes/login.js
+++ src/routes/login.js
@@ -1,10 +1,39 @@
 const express = require('express');
-
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 const router = express.Router();
 const User = require('../models/User');
 
-router.get('/', async (req, res) => {
-    res.send('I m, Login Route Handler');
-});
+router.post('/', async (req, res) => {
+    const {email,password} = req.body;
+    console.log(email,password);
 
+    const user = await User.findOne({email}).select('+password');
+
+    if(!user){
+        return res.status(404).json({message:"Auth failed!!"});
+    }
+
+    await bcrypt.compare(password,user.password)
+        .then(exists =>{
+            if(exists){
+                const token = jwt.sign(
+                    {
+                        id:user._id
+                    },
+                        process.env.JWT_SECRET,
+                    {
+                        expiresIn:'7d'
+                    },
+                );
+                return res.json(token);
+                }
+            else{
+                return res.status(404).json({message:"Auth failed!!"});
+            }
+        })
+
+    
+
+});
 module.exports = router;
