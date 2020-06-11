c0ef74db6edee487cf372757f1bae0062bec7153
Ashutosh619-sudo <Ashutoshsinghrkt@gmail.com>
2020-06-12 02:06:35 +0530

    add plan endpoint

--- src/routes/login.js
+++ src/routes/login.js
@@ -33,7 +33,5 @@ router.post('/', async (req, res) => {
             }
         })
 
-    
-
 });
 module.exports = router;
---
+++ src/routes/plan.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const router = express.Router();
+
+const Plan = require("../models/Plan");
+
+
+router.get('/', async (req,res)=>{
+    await Plan.find()
+        .then((plans)=>{
+            if(plans){
+                res.json(plans);
+            }
+            else{
+                res.status(404).json("Not found!")
+            }
+        })
+        .catch(e=>{
+            console.log(e);
+        })
+})
+
+module.exports = router;
