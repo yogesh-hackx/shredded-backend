f50c17e531296282b547f24a501f81435e502e86
yogesh-hackx <yogesh.hackx@gmail.com>
2020-06-13 14:42:56 +0530

    added name and role fields

--- src/models/User.js
+++ src/models/User.js
@@ -2,20 +2,26 @@ const mongoose = require('mongoose');
 
 const { String } = mongoose.Schema.Types;
 
-const userSchema = new mongoose.Schema(
-    {
-        email: {
-            type: String,
-            required: true,
-            unique: true,
-        },
-        password: {
-            type: String,
-            required: true,
-            select: false,
-        },
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
     },
-);
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+        select: false,
+    },
+    role: {
+        type: Number,
+        default: 0,
+    },
+});
 const User = mongoose.model('User', userSchema);
 
 module.exports = User;
